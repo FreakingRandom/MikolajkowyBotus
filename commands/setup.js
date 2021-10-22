4f949d1949ab7d71194df8b81a1a9de3d4ac3cf2
BuildTools <bory.mode@o2.pl>
2021-10-22 20:08:24 +0200

    a2

--- commands/setup.js
+++ commands/setup.js
@@ -2,29 +2,28 @@ const discord = require('discord.js');
 module.exports.run = async (msg, client, args) => {
     if (msg.member.id !== '285025528442912778' && msg.member.id !== '330768055468818435') {
         msg.delete()
-    } 
-    else {
+    } else {
         msg.delete()
 
         embed = new discord.MessageEmbed()
-        embed.addField(`**Zasady serwera**`,`1.Prosimy o przestrzeganie standardów netykiety.
+        embed.addField(`**Zasady serwera**`, `1.Prosimy o przestrzeganie standardów netykiety.
         2.Próby DoS'owania bota (spam komendami lub spam guzikami) będą karane usunięciem z danej osoby z rozgrywek.
         3.Każdej osobie uczestniczącej powinna zostać przypisana ranga, która pozwala na zobaczenie kanałów z daną grą. Jeśli ranga nie zostanie przypisana bądź bot ulegnie awarii prosimy bezzwłocznie poinformować twórce lub hosta bota (Freaking_Random#3702 - Twórca), (Myster#7218 - Host) na dm.`)
         embed.setTitle(`**Regulamin**`)
         embed.setColor(`fc0303`)
 
         embed2 = new discord.MessageEmbed()
-        embed2.addField('**Uwaga!**',`Klikając jeden z poniższych przycisków akceptujesz regulamin oraz zostanie ci przypisana ranga`)
+        embed2.addField('**Uwaga!**', `Klikając jeden z poniższych przycisków akceptujesz regulamin oraz zostanie ci przypisana ranga`)
         embed2.setTitle(`**Gierki Mikołajkowe**`)
         embed2.setThumbnail(`https://images-ext-2.discordapp.net/external/hCTk9zzzsTfXxFZyPdOs222HND4AfS6JMPWQvYHCgRc/https/cdn-icons-png.flaticon.com/512/621/621904.png`)
         embed2.setColor(`fc0303`)
 
         msg.channel.send({
             embeds: [embed, embed2]
-        
+
         })
 
-        const newrole = ['Rocket League','Counter Strike','Brawlhalla','League of Legends']
+        const newrole = ['Rocket League', 'Counter Strike', 'Brawlhalla', 'League of Legends']
         var rowtable = []
         newrole.forEach(i => {
             const row = new discord.MessageActionRow()
@@ -35,19 +34,19 @@ module.exports.run = async (msg, client, args) => {
                     .setStyle('DANGER')
                 );
             rowtable.push(row)
-            
+
         })
         msg.channel.send({
-                    content: 'Wybierz grę w której chcesz uczestniczyć. ⬇️',
-                    components: rowtable
-                });
+            content: 'Wybierz grę w której chcesz uczestniczyć. ⬇️',
+            components: rowtable
+        });
         client.on('interactionCreate', async interaction => {
             if (!interaction.isButton()) return;
             const role = (await msg.guild.roles.fetch())
             const rolefind = role.find(role => role.name == interaction.customId)
 
             interaction.member.roles.add(rolefind)
-            
+
         });
     }
 }
